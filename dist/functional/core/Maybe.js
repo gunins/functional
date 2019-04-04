07b3e7139806fa20c0f10cf0c86805f82fd33e04
Guntars Simanskis <guntars@diamondgraphics.ie>
2019-04-04 11:24:43 +0100

    Update Option to Maybe

---
+++ dist/functional/core/Maybe.js
@@ -0,0 +1,88 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global['functional/core/Maybe'] = global['functional/core/Maybe'] || {}, global['functional/core/Maybe'].js = {})));
+}(this, (function (exports) { 'use strict';
+
+const some = (value) => new Some(value);
+const none = () => new None();
+
+class Some {
+    constructor(value) {
+        this.value = value;
+    };
+
+    isSome() {
+        return ['[object Some]'].indexOf(this.toString()) !== -1
+    };
+
+    isOption() {
+        return ['[object Some]', '[object None]'].indexOf(this.toString()) !== -1;
+    }
+
+    get() {
+        return this.value;
+    };
+
+    map(fn) {
+        return this.isSome() ? some(fn(this.get())) : none();
+    };
+
+    flatMap(fn) {
+        const out = fn(this.get());
+        if (out.isOption) {
+            return out;
+        }else{
+            throw new ReferenceError('Must return an Option');
+        }
+
+    }
+
+    set(value) {
+        return some(value);
+    };
+
+    isEmpty() {
+        return this.value ? false : true;
+    };
+
+    getOrElse(defaultVal) {
+        return this.isSome() ? this.value : defaultVal
+    };
+
+    getOrElseLazy(defaultVal=()=>{}) {
+        return this.isSome() ? this.value : defaultVal()
+    };
+
+    toString() {
+        return '[object Some]';
+    };
+}
+
+class None extends Some {
+    constructor() {
+        super();
+    };
+
+    isSome() {
+        return false;
+    };
+
+    set(value) {
+        return none();
+    };
+
+    toString() {
+        return '[object None]';
+    };
+
+}
+
+exports.Some = Some;
+exports.some = some;
+exports.None = None;
+exports.none = none;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
